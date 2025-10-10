558a785d5bd591dc5e399e1b5eaf3e8d7b171cf7
Cote <marcastillo2021@udec.cl>
2025-10-10 00:11:25 -0300

    Update variable names from recetum to receta
    Renamed variables and references from 'recetum' to 'receta' in edit.php to reflect correct entity naming and improve code clarity.

--- backend/templates/Receta/edit.php
+++ backend/templates/Receta/edit.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @var \App\View\AppView $this
- * @var \App\Model\Entity\Recetum $recetum
+ * @var \App\Model\Entity\Receta $receta
  * @var string[]|\Cake\Collection\CollectionInterface $producto
  */
 ?>
@@ -11,17 +11,17 @@
             <h4 class="heading"><?= __('Actions') ?></h4>
             <?= $this->Form->postLink(
                 __('Delete'),
-                ['action' => 'delete', $recetum->id_receta],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $recetum->id_receta), 'class' => 'side-nav-item']
+                ['action' => 'delete', $receta->id_receta],
+                ['confirm' => __('Are you sure you want to delete # {0}?', $receta->id_receta), 'class' => 'side-nav-item']
             ) ?>
             <?= $this->Html->link(__('List Receta'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column column-80">
         <div class="receta form content">
-            <?= $this->Form->create($recetum) ?>
+            <?= $this->Form->create($receta) ?>
             <fieldset>
-                <legend><?= __('Edit Recetum') ?></legend>
+                <legend><?= __('Edit Receta') ?></legend>
                 <?php
                     echo $this->Form->control('nombre');
                     echo $this->Form->control('descripcion');
